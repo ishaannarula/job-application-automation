75e352137ca010019994196680ddce4e64077d7a
ishaan-narula <60092550+ishaan-narula@users.noreply.github.com>
2022-03-04 18:27:42 +0530

    Edits

--- record-creation-routine.py
+++ record-creation-routine.py
@@ -1,22 +1,29 @@
-# Create folder
 import os
-directory = 'enter folder name for job opening (e.g. Company Name - Position Name - Job ID - Location'
-parent_dir = '/Volumes/GoogleDrive/My Drive/Career/Search/Applications/To be Added to Excel Database/'
-path = os.path.join(parent_dir, directory)
-os.mkdir(path)
-
-# Add CV files
-cv_location = 'enter file path'
-cv_files = ['fname.docx', 'fname.pdf']
-
 import shutil
-for f in cv_files:
-    src = os.path.join(cv_location, f)
-    shutil.copy2(src, path)
-
-# Add resulting folder to favourites
 from finder_sidebar_editor import FinderSidebar
-FinderSidebar().add(path)
+
+dirLst = []
+
+parent_dir = 'enter file path'
+
+for dir in dirLst:
+    # Create folder
+    path = os.path.join(parent_dir, dir)
+    os.mkdir(path)
+
+    # Add CV files
+    cv_location = 'enter file path'
+    cv_files = ['CV_Ishaan Narula.docx', 'CV_Ishaan Narula.pdf']
+
+    for f in cv_files:
+        src = os.path.join(cv_location, f)
+        shutil.copy2(src, path)
+
+    # Add resulting folder to favourites
+    FinderSidebar().add(path)
+
+
+
 
 # Rough
 #print(os.path.abspath(directory))
